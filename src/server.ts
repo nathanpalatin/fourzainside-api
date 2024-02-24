435b243415ebfb617076b7f09a8e87117711cebf
Nathan Palatin <nath.palatin@gmail.com>
2024-02-24 10:39:01 -0300

    [fix] corrigido problema de eslint reclamando.

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-
 app.get('/', () => {
-  return "Hello world!"
+  return 'Hello world!'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('Servidor iniciado')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Servidor iniciado')
+  })
