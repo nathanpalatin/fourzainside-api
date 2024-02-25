f138585813f68838a149dcb71198ae00dc307b61
Nathan Palatin <nath.palatin@gmail.com>
2024-02-25 14:18:35 -0300

    [chore] alterado para sqlite e fazer testes e criado a primeira migration para transações.

--- src/database.ts
+++ src/database.ts
@@ -5,19 +5,20 @@ import { configDotenv } from 'dotenv'
 configDotenv()
 
 export const config: Knex.Config = {
-  client: 'pg',
+  client: 'sqlite3',
   connection: {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
+    /*  host: process.env.HOST,
+     port: process.env.PORT,
+     user: process.env.USER,
+     password: process.env.PASSWORD,
+     database: process.env.DB_NAME, */
     filename: './db/app.db'
   },
   migrations: {
     extension: 'ts',
     directory: 'db/migrations'
   },
+  useNullAsDefault: true
 }
 
 export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = knex('postgres').select('*')
+  const tables = knex('sqlite_schema').select('*')
 
   return tables
 })
