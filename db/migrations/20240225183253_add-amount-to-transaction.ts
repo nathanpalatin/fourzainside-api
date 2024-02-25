67f9240e6e86d40f27bcc4a9becd505a58716f22
Nathan Palatin <nath.palatin@gmail.com>
2024-02-25 15:37:28 -0300

    [chore] adicionado coluna amount na tabela.

---
+++ db/migrations/20240225183253_add-amount-to-transaction.ts
@@ -0,0 +1,16 @@
+import type { Knex } from 'knex'
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.decimal('amount', 10, 2).after('id').notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('amount')
+  })
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
+app.get('/db', async () => {
+  const transaction = knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transaction test',
+    amount: 1000,
+  })
 
-  return tables
+  return transaction
 })
 
 app
