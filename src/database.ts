c383eeab4ff1405bd468a4325b921fe4c495507b
Nathan Palatin <nath.palatin@gmail.com>
2024-02-25 14:02:44 -0300

    [fix] organizado pasta de migrations.

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,23 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
 import { configDotenv } from 'dotenv'
 
 configDotenv()
 
-export const config = {
+export const config: Knex.Config = {
   client: 'pg',
   connection: {
     host: process.env.HOST,
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DB_NAME
-  }
+    database: process.env.DB_NAME,
+    filename: './db/app.db'
+  },
+  migrations: {
+    extension: 'ts',
+    directory: 'db/migrations'
+  },
 }
 
 export const knex = setupKnex(config)
