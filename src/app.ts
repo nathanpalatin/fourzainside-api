0ccbe4ed033b23a673bdfb27753984da146894c6
Nathan Palatin <nath.palatin@gmail.com>
2024-09-07 17:34:45 -0300

    feat: adicionado Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,17 @@ import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
+import { ZodError } from 'zod'
+
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	ZodTypeProvider
+} from 'fastify-type-provider-zod'
 
 import { postsRoutes } from './routes/posts'
 import { usersRoutes } from './routes/users'
@@ -12,9 +23,7 @@ import { productsRoutes } from './routes/products'
 import { transactionsRoutes } from './routes/transactions'
 import { notificationsRoutes } from './routes/notifications'
 
-import { ZodError } from 'zod'
-
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET_KEY,
@@ -25,6 +34,24 @@ app.register(fastifyJwt, {
 	sign: {
 		expiresIn: '30m'
 	}
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'API NODE Rest',
+			description: 'by Nathan Palatin.',
+			version: '1.0.0'
+		}
+	},
+	transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUI, {
+	routePrefix: '/docs'
 })
 
 app.register(cookie)
