fd41793b54b3dbc6447d8100e06319037f8f26a6
Nathan Palatin <nath.palatin@gmail.com>
2024-10-25 10:21:39 -0300

    ci: list all courses from user use case.

---
+++ src/use-cases/courses/get-course-by-user.spec.ts
@@ -0,0 +1,22 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { randomUUID } from 'crypto'
+import { GetCourseByUserUseCase } from './get-course-by-user'
+import { InMemoryCoursesRepository } from '../../repositories/in-memory/in-memory-courses-repository'
+
+let notificationsRepository: InMemoryCoursesRepository
+let sut: GetCourseByUserUseCase
+
+describe('Get Courses by User Use Case', () => {
+	beforeEach(() => {
+		notificationsRepository = new InMemoryCoursesRepository()
+		sut = new GetCourseByUserUseCase(notificationsRepository)
+	})
+
+	it('should be able to get all courses by user', async () => {
+		const { courses } = await sut.execute({
+			userId: randomUUID()
+		})
+
+		expect(courses).toBeTruthy()
+	})
+})
